81213af01bdf421def3b6b98853c463552181f91
Jay Alvin C. Vallescas <202080209@psu.palawan.edu.ph>
2022-09-20 22:15:00 +0800

    web5 done

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,27 @@
 <?php
+session_start();
+if(isset($_SESSION['num_visits'])){
+   $_SESSION['num_visits']+=1; 
+
+   if($_SESSION['num_visits'] == 5){
+      echo "Thank you for visiting the page a lot.";
+   }
+   else if($_SESSION['num_visits'] == 10){
+      header("Location: congratulations.php");
+      exit();
+   }
+} else {
+   $_SESSION['num_visits']=1; 
+}
+
+if(isset($_GET['reset'])){
+   session_destroy();
+   header("Location: index.php");
+   exit();
+} 
 
 ?>
 
 <h1>Hello</h1>
-<p>You have visited this page <?= $num_visits ?> times</p>
+<p>You have visited this page <?php print_r($_SESSION['num_visits']); ?> times</p>
 <p><a href="?reset=true">Reset</a></p>
