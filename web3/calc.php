ba2f4b82f84c1e1bed687031e5308a33671096d9
Jay Alvin C. Vallescas <202080209@psu.palawan.edu.ph>
2022-09-26 17:04:29 +0800

    web3 done

--- web3/calc.php
+++ web3/calc.php
@@ -3,17 +3,24 @@
 <body>
     <?php
         # test if input is_numeric
-        if(1){
+        if(isset($_POST['val1']) and is_numeric($_POST['val1']) and 
+           isset($_POST['val2']) and is_numeric($_POST['val2'])){
+            $val1 = $_POST['val1'];
+            $val2 = $_POST['val2'];
             # test if calc is not null
-            if(1){
+            if(isset($_POST['calc']) and !empty($_POST['calc'])){
+                $calc = $_POST['calc'];
                 # switch statement for value of calc
-                switch(1){
-                    
-                }
+                switch($calc){
+                    case 'add': $result = $val1 + $val2; break;
+                    case 'sub': $result = $val1 - $val2; break;
+                    case 'mul': $result = $val1 * $val2; break;
+                    case 'div': $result = $val1 / $val2; break;
+                    default: echo "invalid";
+                } 
                 echo "Calculation result: $result";
-            }
-        }
-        else{ echo "Invalid entry - please retry"; }
+            } else {echo "Operator is missing, please choose an operator";}
+        } else {echo "Invalid entry - please retry"; }
     ?>
 </body>
 </html>
